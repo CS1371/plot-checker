3d2c63a97cf069a7f11d614e1201934247ab99b0
Alexander Rao <arao81@gatech.edu>
2018-07-16 16:36:13 -0400

    Remove Stress Test because it adds no value

--- unitTests/stressTest_test.m
+++
@@ -1,9 +0,0 @@
-%% Stress Test: stress test the plot checker
-%
-% generate 100 plots, all equal
-function [passed, msg] = stressTest_test
-    [passed, msg] = checkPlots(@stressTest, [10 10], 30:500, 1);
-    if ~passed
-        msg = sprintf('Expected passing; got %s', msg);
-    end
-end
